fc515f1d78a87dfb91880a93672dd9b7ea83d151
Jeff Keen <jeff@keen.me>
2017-08-23 14:48:20 -0500

    Add --ids flag

--- index.js
+++ index.js
@@ -86,6 +86,7 @@
       .version('0.0.1')
       .option('-d, --debug', 'Turn on debugging')
       .option('-l, --limit [value]', 'Only return the last X records')
+      .option('-i, --ids [value]', 'Only return messages with ids')
       .option('-s, --sinceDate [YYYY-MM-DD]', 'Only return records since date')
       .option('-f, --search [value]', 'Only return records containing text')
       .option('-p, --phone [value]', 'Only return records to/from number')
@@ -97,15 +98,16 @@
     if (options.limit) console.log(`limited to ${options.limit}`);
     if (options.sinceDate) console.log(`only getting entries since ${options.sinceDate}`);
     if (options.search) console.log(`only getting entries containing ${options.search}`);
-    if (options.phone) console.log(`only getting entires containing ${options.phone}`);
+    if (options.ids) console.log(`only getting message ids ${options.ids}`);
+    if (options.phone) console.log(`only getting to/from ${options.phone}`);
     if (options.test) console.log(`only testing entires`);
     if (options.save) console.log(`writing to ${path.join(__dirname, "data.json").toString()}`);
 
     options.showProgress = true; // don't do this for when using this not on the command line
 
     exporter.importData(expandHomeDir(filePath), options).then(data => {
-      if (options.save) {
       if (options.test) {
+        console.log(prettyoutput(data.validations, {maxDepth: 7}));
         if (options.save) {
           bfj.write(`${path.join(__dirname, "validations.json").toString()}`, data.validations, {space: 4})
             .then(() => {
